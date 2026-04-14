8f677962593f67f13af0de9294688df7d1b1c903
septian <septianari@hotmail.com>
2026-04-14 21:38:16 +0700

    Updates

--- _next/static/M6OjTvzFGDums5molA07V/_buildManifest.js
+++ _next/static/M6OjTvzFGDums5molA07V/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/porto//_next/:path+",
+        "destination": "/porto/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
